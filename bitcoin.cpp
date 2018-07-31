eab46ce6c3168be66d1c3613b6a87a8eda7b0122
Stasiu Chyczewski <schyczewski@gmail.com>
2018-07-30 20:18:33 -0400

    Ports changed

--- bitcoin.cpp
+++ bitcoin.cpp
@@ -298,7 +298,7 @@ bool TestNode(const CService &cip, int &ban, int &clientV, std::string &clientSV
 
 /*
 int main(void) {
-  CService ip("sumcointools.com", 9333, true);
+  CService ip("sumcointools.com", 3333, true);
   vector<CAddress> vAddr;
   vAddr.clear();
   int ban = 0;
