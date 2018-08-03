4bbd2e03d19a0631dba12cf95710ea32fb9a7fe2
Stasiu Chyczewski <schyczewski@gmail.com>
2018-08-02 22:00:00 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -400,7 +400,7 @@ extern "C" void* ThreadStats(void*) {
   return nullptr;
 }
 
-static const string mainnet_seeds[] = {"dnsseed.sumcoinpool.org", "dnsseed.sumcoinwallet.org", ""};
+static const string mainnet_seeds[] = {"127.0.0.1", ""};
 static const string testnet_seeds[] = {"ahhhhh", ""};
 static const string *seeds = mainnet_seeds;
 
