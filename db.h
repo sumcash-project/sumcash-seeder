bf5698a4e417cd6eb768dbf49b2e1e9da64029da
5053s <51257210+5053s@users.noreply.github.com>
2020-06-10 00:15:54 -0600

    set mainnet to block 1150000
    suggested change/update after Aqua vid https://youtu.be/DsaxbwwVEXk?t=325

--- db.h
+++ db.h
@@ -16,7 +16,7 @@
 
 static inline int GetRequireHeight(const bool testnet = fTestNet)
 {
-    return testnet ? 100 : 200;
+    return testnet ? 100 : 1150000;
 }
 
 std::string static inline ToString(const CService &ip) {
